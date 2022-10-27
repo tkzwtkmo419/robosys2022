97c75adc683f06a5a192cad3a5504be744c2d70e
Takumi Takazawa <tkzwpen0419@outlook.jp>
2022-10-27 12:28:06 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,7 +1,19 @@
 #!/bin/bash
+#SPDX-FileCopyrightText: 2022 Takumi Takazawa tkzwpen0419@outlook.jp
+#SPDX-License=Identifier: GPL-3.0
+
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
 
 out=$(seq 5 | ./plus)
 
-[ "${out}" = 14 ]
+[ "${out}" = 15 ] || ng ${LINENO}
 
+[ "$res" = 0 ] && echo OK
+exit $res
 
