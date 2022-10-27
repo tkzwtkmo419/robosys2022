dbaae3743e042783ac9ef83a26339fc5326e492e
Takumi Takazawa <tkzwpen0419@outlook.jp>
2022-10-27 12:48:10 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 #SPDX-FileCopyrightText: 2022 Takumi Takazawa tkzwpen0419@outlook.jp
 #SPDX-License=Identifier: GPL-3.0
 
@@ -9,10 +9,19 @@ ng () {
 
 res=0
 
-
+### I/O TEST ###
 out=$(seq 5 | ./plus)
 
 [ "${out}" = 15 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
