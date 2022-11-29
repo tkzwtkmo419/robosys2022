e979a49ff3ee646c9f79d9ef09985ee0760bd093
Takumi Takazawa <tkzwpen0419@outlook.jp>
2022-11-30 08:53:36 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,6 +1,6 @@
 #!/bin/bash -xv
 #SPDX-FileCopyrightText: 2022 Takumi Takazawa tkzwpen0419@outlook.jp
-#SPDX-License=Identifier: GPL-3.0
+#SPDX-License=Identifier: BSD-3-Clause
 
 ng () {
 	echo NG at Line $1
